ab8d567678d1e1ade37c54cf60266db52ca37ba4
bonniefox <bonnie_huhui@163.com>
2017-10-14 16:00:44 +0800

    Update testAddHandler.java

--- bingo/testAddHandler.java
+++ bingo/testAddHandler.java
@@ -1,15 +1,15 @@
-import static org.junit.Assert.*;
+  import static org.junit.Assert.*;
 public class TestDefaultController
 {
   @Test
-public class TestDefaultController
-{
- Request request = new SampleRequest();
- RequestHandler handler = new SampleHandler();
- controller.addHandler(request,handler);
- RequestHandler handler2 = controller.getHandler(request);
- assertSame("Handler we set in controller should be the same handler we get",handler2,handler);
+   public class TestDefaultController
+   {
+      Request request = new SampleRequest();
+      RequestHandler handler = new SampleHandler();
+      controller.addHandler(request,handler);
+      RequestHandler handler2 = controller.getHandler(request);
+      assertSame("Handler we set in controller should be the same handler we get",handler2,handler);
 
-}
+  }
 
 }

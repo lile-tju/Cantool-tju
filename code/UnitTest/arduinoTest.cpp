27567d4e4317dfe3c57a70d87e08922ec9ee6c3a
tjhxwbp <tjhxwbp@outlook.com>
2017-10-25 22:53:00 +0800

    Add files via upload

---
+++ code/UnitTest/arduinoTest.cpp
@@ -0,0 +1,12 @@
+// arduinoTest.cpp
+//
+
+#include "stdafx.h"
+#include "gtest\gtest.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+

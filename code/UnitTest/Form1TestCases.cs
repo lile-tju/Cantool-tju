5a184677f07dc9277f82c50a093c1af60d1afbe2
tjhxwbp <tjhxwbp@outlook.com>
2017-10-30 13:54:43 +0800

    Add files via upload

--- code/UnitTest/Form1TestCases.cs
+++ code/UnitTest/Form1TestCases.cs
@@ -1,161 +1,118 @@
-﻿using unitTest;
-using IniOperate;
+﻿using IniOperate;
+using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Xunit;
-using Shouldly;
 
-namespace unitTest
+namespace CanTool
 {
     public class Form1TestCases
     {
-        [Fact]
+        //静态方法测试
+        //[Fact]
+        //测试写INI文件
+        //public void ReadIniDataTest()
+        //{
+        //    var oif = new OperateIniFile();
+        //    Assert.NotNull(oif.ReadIniData("0000", "0000", "0000", "data.txt"));
+        //}
+
+        //[Fact]
+        //测试读INI文件
+        //public void WriteIniDataTest()
+        //{
+        //    var oif = new OperateIniFile();
+        //    oif.WriteIniData("0000", "0000", "0000", "data.txt").ShouldBe(false);
+        //}
+
+
+        //点击事件测试
         //
+
+        [Fact]
+        //测试打开按钮点击事件
         public void openButton_ClickTest()
         {
             var fm = new Form1();
-            fm.openButton_Click().ShouldBe(1);
+            fm.openButton_Click(new object(), new EventArgs());
         }
 
         [Fact]
-        //
-        public void openButton_ClickTest_1()
-        {
-            var fm = new Form1();
-            fm.openButton_Click().ShouldBe(2);
-        }
-
-        [Fact]
-        //
+        //测试关闭按钮点击事件
         public void closeButton_ClickTest()
         {
             var fm = new Form1();
-            fm.closeButton_Click().ShouldBe(1);
-        }
-
-        [Fact]
-        //
-        public void closeButton_ClickTest_1()
-        {
-            var fm = new Form1();
-            fm.closeButton_Click().ShouldBe(2);
-        }
-
-        [Fact]
-        //
-        public void Form1_FormClosed1Test()
-        {
-            var fm = new Form1();
-            fm.Form1_FormClosed1().ShouldBe(1);
-        }
-
-        [Fact]
-        //
-        public void Form1_FormClosed1Test_1()
-        {
-            var fm = new Form1();
-            fm.Form1_FormClosed1().ShouldBe(0);
+            fm.closeButton_Click(new object(), new EventArgs());
         }
 
 
         [Fact]
-        //
+        //测试发送按钮点击事件
         public void SendButton_ClickTest()
         {
             var fm = new Form1();
-            fm.SendButton_Click().ShouldBe(1);
+            fm.SendButton_Click(new object(), new EventArgs());
         }
 
         [Fact]
-        //
-        public void SendButton_ClickTest_1()
+        //测试Canfrom点击事件
+        public void Canform_ClickTest()
         {
             var fm = new Form1();
-            fm.SendButton_Click().ShouldBe(2);
-        }
-
-
-        [Fact]
-        //
-        public void SetVisiable1Test()
-        {
-            var fm = new Form1();
-            fm.SetVisiable1(1).ShouldBe(1);
+            fm.Canform_Click(new object(), new EventArgs());
         }
 
         [Fact]
-        //
-        public void SetVisiable1Test_1()
-        {
-            var fm = new Form1();
-            fm.SetVisiable1(0).ShouldBe(0);
-        }
-
-        [Fact]
-        //
+        //测试接受版本信息点击事件
         public void getversionbutton_ClickTest()
         {
             var fm = new Form1();
-            fm.getversionbutton_Click().ShouldBe(1);
+            fm.getversionbutton_Click(new object(), new EventArgs());
         }
 
-        [Fact]
-        //
-        public void getversionbutton_ClickTest_1()
-        {
-            var fm = new Form1();
-            fm.getversionbutton_Click().ShouldBe(2);
-		}
 
         [Fact]
-        //
+        //测试打开CanTool按钮点击事件
         public void openCanToolbutton_ClickTest()
         {
             var fm = new Form1();
-            fm.openCanToolbutton_Click().ShouldBe(1);
+            fm.openCanToolbutton_Click(new object(), new EventArgs());
         }
 
         [Fact]
-        //
-        public void openCanToolbutton_ClickTest_1()
-        {
-            var fm = new Form1();
-            fm.openCanToolbutton_Click().ShouldBe(2);
-        }
-
-        [Fact]
-        //
+        //测试设置速率按钮点击事件
         public void setSnbutton_ClickTest()
         {
             var fm = new Form1();
-            fm.setSnbutton_Click().ShouldBe(1);
+            fm.setSnbutton_Click(new object(), new EventArgs());
         }
 
         [Fact]
-        //
-        public void setSnbutton_ClickTest_1()
-        {
-            var fm = new Form1();
-            fm.setSnbutton_Click().ShouldBe(2);
-        }
-
-        [Fact]
-        //
+        //测试关闭CanTool按钮点击事件
         public void closeCanToolbutton_ClickTest()
         {
             var fm = new Form1();
-            fm.closeCanToolbutton_Click().ShouldBe(1);
+            fm.closeCanToolbutton_Click(new object(), new EventArgs());
         }
 
         [Fact]
-        //
-        public void closeCanToolbutton_ClickTest_1()
+        //测试展示窗口按钮点击事件
+        public void showconwindbutton_ClickTest()
         {
             var fm = new Form1();
-            fm.closeCanToolbutton_Click().ShouldBe(2);
+            fm.showconwindbutton_Click(new object(), new EventArgs());
+        }
+
+        [Fact]
+        //测试加载按钮点击事件
+        public void Form1_LoadTest()
+        {
+            var fm = new Form1();
+            fm.Form1_Load(new object(), new EventArgs());
         }
     }
 }
